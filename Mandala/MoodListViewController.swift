58504abccecfdc252c2a39d2e7c4c830c6a9b1af
dirk <dirkadin@gmail.com>
2018-08-20 16:10:09 -0500

    Added a container view with it's controller

---
+++ Mandala/MoodListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MoodListViewController.swift
+//  Mandala
+//
+//  Created by Dirk on 8/20/18.
+//  Copyright © 2018 Dirk. All rights reserved.
+//
+
+import UIKit
+
+class MoodListViewController: UITableViewController {
+	
+	private var moodEntries: [MoodEntry] = []
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return moodEntries.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		let moodEntry = moodEntries[indexPath.row]
+		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+		
+		cell.imageView?.image = moodEntry.mood.image
+		cell.textLabel?.text = "I was \(moodEntry.mood.name)"
+		
+		let dateString = DateFormatter.localizedString(from: moodEntry.timeStamp,
+													   dateStyle: .medium,
+													   timeStyle: .short)
+		
+		cell.detailTextLabel?.text = "on \(dateString)"
+		
+		return cell
+		
+	}
+	
+}
